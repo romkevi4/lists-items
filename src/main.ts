34d768b1d7a968560e56b2302b4576c7d19083a6
unknown <shatskikh.roman@gmail.com>
2023-10-12 01:33:39 +0300

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
 import './assets/main.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.mount('#app')
+app
+  .use(pinia)
+  .mount('#app')
